72662a6877c6ce2c19b1bbd40abb4fd44a23c9c7
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2024-01-09 22:04:35 +0700

    fix failed build

--- src/components/homepage/carousel.jsx
+++ src/components/homepage/carousel.jsx
@@ -5,15 +5,15 @@ const ImageCarousel = () => {
 	return (
 		<Carousel>
 			<div>
-				<img src="IMG_0778.JPG" />
+				<img src="IMG_0778.JPG" alt="Legend 1" />
 				<p className="legend">Legend 1</p>
 			</div>
 			<div>
-				<img src="IMG_0780.JPG" />
+				<img src="IMG_0780.JPG" alt="Legend 2" />
 				<p className="legend">Legend 2</p>
 			</div>
 			<div>
-				<img src="IMG_0781.JPG" />
+				<img src="IMG_0781.JPG" alt="Legend 3" />
 				<p className="legend">Legend 3</p>
 			</div>
 		</Carousel>
