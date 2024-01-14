b53642c5964cbaacecc679782232572df9ecb87b
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2024-01-14 21:54:18 +0700

    fix failed build

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4";
 
 import Homepage from "./pages/homepage";
-import About from "./pages/about";
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
 
